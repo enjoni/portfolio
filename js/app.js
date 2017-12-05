8c78bce830653fde7b453872afdced94f896fb27
enjoni <jo.wiederkehr@gmail.com>
2017-12-05 10:46:09 +0100

    first

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// $(document).foundation()
+// $(document).ready(function () {
+//     setInterval(function(){console.log("reload is ready!");
+  
+//   var winWidth = $(window).width();
+//   var winHeight = $(window).height();
+  
+//   $(window).resize(function(){
+//   });
+  
+//   $('.border').css({
+//   'height': winHeight*0.9,
+//    'width': winWidth*0.9,
+//   'margin-top': winHeight*0.05,
+//   'margin-left': winHeight*0.05,
+//   'margin-right': winHeight*0.05,  
+//   'margin-bottom': winHeight*0.05,
+  
+//   });},10);
+  
+  
+//   });//READY
