fac12cf5a10343cbca40473fa9349a307534c683
enjoni <jo.wiederkehr@gmail.com>
2017-12-31 14:19:38 +0100

    31.12

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
-// $(document).foundation()
-// $(document).ready(function () {
-//     setInterval(function(){console.log("reload is ready!");
+$(document).foundation()
+$(document).ready(function () {
+    setInterval(function(){console.log("reload is ready!");
   
-//   var winWidth = $(window).width();
-//   var winHeight = $(window).height();
+  var winWidth = $(window).width();
+  var winHeight = $(window).height();
   
-//   $(window).resize(function(){
-//   });
+  $(window).resize(function(){
+  });
   
-//   $('.border').css({
-//   'height': winHeight*0.9,
-//    'width': winWidth*0.9,
-//   'margin-top': winHeight*0.05,
-//   'margin-left': winHeight*0.05,
-//   'margin-right': winHeight*0.05,  
-//   'margin-bottom': winHeight*0.05,
+  $('.border').css({
+  'height': winHeight*0.9,
+   'width': winWidth*0.9,
+  'margin-top': winHeight*0.05,
+  'margin-left': winHeight*0.05,
+  'margin-right': winHeight*0.05,  
+  'margin-bottom': winHeight*0.05,
   
-//   });},10);
-  
-  
-//   });//READY
+  });},10);
+
+  });//READY
